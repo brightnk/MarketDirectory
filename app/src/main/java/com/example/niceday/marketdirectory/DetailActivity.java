92f2b48483933187de954ab780c245c0ebf466bc
Jiyun <brightnk@outlook.com>
2017-11-09 21:21:30 -0800

    complete detailActivity layout

--- app/src/main/java/com/example/niceday/marketdirectory/DetailActivity.java
+++ app/src/main/java/com/example/niceday/marketdirectory/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.niceday.marketdirectory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Market selectedMarket = (Market)getIntent().getSerializableExtra("SelectedMarket");
         Log.d("DetailActivity", selectedMarket.marketName);
+
+        ((TextView)findViewById(R.id.marketNameTxt)).setText(selectedMarket.marketName);
+        ((TextView)findViewById(R.id.marketIdTxt)).setText(String.valueOf("Market ID: " + selectedMarket.id));
+        ((TextView)findViewById(R.id.marketDistanceTxt)).setText(String.valueOf(" Distance: " + selectedMarket.distance));
+        ((TextView)findViewById(R.id.marketAdressTxt)).setText("Address: \n" + selectedMarket.marketDetail.address);
+        ((TextView)findViewById(R.id.marketLinkTxt)).setText("The Google Link: \n"+selectedMarket.marketDetail.googleLink);
+        ((TextView)findViewById(R.id.marketProductTxt)).setText("Products: \n"+selectedMarket.marketDetail.products);
+        ((TextView)findViewById(R.id.marketScheduleTxt)).setText("Open Hours: \n"+selectedMarket.marketDetail.schedule);
 
 
     }

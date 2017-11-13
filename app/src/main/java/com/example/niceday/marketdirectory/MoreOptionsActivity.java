05f8d139f83041801793524c2c1ea464ac5a684e
Jiyun <brightnk@outlook.com>
2017-11-12 23:27:30 -0800

    update more option activity layout

--- app/src/main/java/com/example/niceday/marketdirectory/MoreOptionsActivity.java
+++ app/src/main/java/com/example/niceday/marketdirectory/MoreOptionsActivity.java
@@ -1,5 +1,6 @@
 package com.example.niceday.marketdirectory;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MoreOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_options);
+
+        boolean istablet = getResources().getBoolean(R.bool.isTablet);
+        if(istablet){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+
     }
 }
